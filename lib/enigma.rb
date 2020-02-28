6a3b96b9cd1c4983f3c9547ab51ef755fb169a88
Zach Holcomb <zachholcombmusic@gmail.com>
2020-02-27 21:34:24 -0700

    Add shift_generator to enigma, pass tests

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,2 +1,7 @@
 class Enigma
+  attr_reader :shift_generator
+
+  def initialize
+    @shift_generator = ShiftGenerator.new
+  end
 end
--- test/enigma_test.rb
+++ test/enigma_test.rb
@@ -1,11 +1,19 @@
 require_relative 'test_helper'
+require './lib/enigma'
+require './lib/shift_generator'
 
 class EnigmaTest < Minitest::Test
   def setup
+    @shift_generator = ShiftGenerator
     @enigma = Enigma.new
   end
 
   def test_it_exists
     assert_instance_of Enigma, @enigma
   end
+
+  def test_it_has_attributes
+    assert_instance_of ShiftGenerator, @enigma.shift_generator
+  end
+
 end
