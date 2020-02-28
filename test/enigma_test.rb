7ab6864dd083e758fb3c41a12abd009622ce2e60
Zach Holcomb <zachholcombmusic@gmail.com>
2020-02-27 21:26:23 -0700

    Add test it exists test to enigma test and pass test

---
+++ test/enigma_test.rb
@@ -0,0 +1,11 @@
+require_relative 'test_helper'
+
+class EnigmaTest < Minitest::Test
+  def setup
+    @enigma = Enigma.new
+  end
+
+  def test_it_exists
+    assert_instance_of Enigma, @enigma
+  end
+end
