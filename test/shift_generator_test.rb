87a76cfa41ec7a2aa49ec7c558753dc98dd4de30
Zach Holcomb <zachholcombmusic@gmail.com>
2020-02-27 18:48:31 -0700

    Make test for splitting random number into smaller keys, not sure if value should be hash, but using hash as return value for now

--- test/shift_generator_test.rb
+++ test/shift_generator_test.rb
@@ -14,4 +14,10 @@ class ShiftGeneratorTest < Minitest::Test
     assert_instance_of String, @shift_generator.generate_random_key
     assert_equal 5, @shift_generator.generate_random_key.to_s.length
   end
+
+  def test_it_can_split_return_value_of_generate_random_key_into_separate_keys
+    @shift_generator.stubs(:generate_random_key).returns("02715")
+    expected = { :keyA => "02", :keyB => "27", :keyC => "71", :keyD => "15"}
+    assert_equal expected, @shift_generator.separate_random_key
+  end
 end
