10091bb4ef52bcd0ddf17ffdd5d7231f4e21bac8
Zach Holcomb <zachholcombmusic@gmail.com>
2020-02-27 22:39:29 -0700

    Start making encrypt test for enigma class test

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,7 +1,8 @@
 class Enigma
-  attr_reader :shift_generator
+  attr_reader :shift_generator, :character_set
 
   def initialize
+    @character_set = ("a".."z").to_a << " "
     @shift_generator = ShiftGenerator.new
   end
 end
