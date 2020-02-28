fbbb6ee1a6bc3294f1a2935f2af5e5238e16d567
Zach Holcomb <zachholcombmusic@gmail.com>
2020-02-27 19:33:58 -0700

    Add attr_reader methods for shift_generator keys and dates

--- lib/shift_generator.rb
+++ lib/shift_generator.rb
@@ -1,9 +1,26 @@
+require 'date'
+
 class ShiftGenerator
+  attr_reader :key, :date
+
+  def initialize
+    @key = generate_random_key
+    @date = Date.today.strftime('%d%m%y')
+  end
+
   def generate_random_key
     rand_num = ""
     until rand_num.length == 5
       rand_num += rand(10).to_s
     end
     rand_num
+  end
+
+  def separate_random_key(five_digit_key)
+    { keyA: five_digit_key[0..1],
+      keyB: five_digit_key[1..2],
+      keyC: five_digit_key[2..3],
+      keyD: five_digit_key[3..4]
+    }
   end
 end
--- test/shift_generator_test.rb
+++ test/shift_generator_test.rb
@@ -3,11 +3,19 @@ require './lib/shift_generator'
 
 class ShiftGeneratorTest < Minitest::Test
   def setup
+    Date.stubs(:today).returns(Date.new(2020, 02, 26))
     @shift_generator = ShiftGenerator.new
   end
 
   def test_it_exists
     assert_instance_of ShiftGenerator, @shift_generator
+  end
+
+  def test_it_has_attributes
+    @shift_generator.stubs(:generate_random_key).returns("02715")
+    assert_instance_of String, @shift_generator.key
+    assert_equal 5, @shift_generator.key.length
+    assert_equal "260220", @shift_generator.date
   end
 
   def test_it_can_generate_random_five_digit_number_key
@@ -16,8 +24,15 @@ class ShiftGeneratorTest < Minitest::Test
   end
 
   def test_it_can_split_return_value_of_generate_random_key_into_separate_keys
-    @shift_generator.stubs(:generate_random_key).returns("02715")
     expected = { :keyA => "02", :keyB => "27", :keyC => "71", :keyD => "15"}
-    assert_equal expected, @shift_generator.separate_random_key
+    assert_equal expected, @shift_generator.separate_random_key("02715")
+
+    random_test_key = @shift_generator.generate_random_key
+    assert_instance_of Hash, @shift_generator.separate_random_key(random_test_key)
+    assert_equal 4, @shift_generator.separate_random_key(random_test_key).values.length
+  end
+
+  def test_it_can_get_offset_keys_by_todays_date
+
   end
 end
