c345ff5ece54eabc75ea56e261e264888320056b
Zach Holcomb <zachholcombmusic@gmail.com>
2020-03-02 20:44:22 -0700

    Remove pry from crack.rb

--- runner_files/crack.rb
+++ runner_files/crack.rb
@@ -3,7 +3,6 @@ require './lib/enigma'
 enigma = Enigma.new
 message_reader = File.open(ARGV[0], "r")
 message_to_decrypt = message_reader.read.chomp
-require "pry"; binding.pry
 decrypted_file = File.open(ARGV[1], "w")
 output = enigma.crack(message_to_decrypt, ARGV[2])
 decrypted_file.write(output[:decryption])
