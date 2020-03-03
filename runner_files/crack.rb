08a1976998abe1e30aefb41294e0ebf2fe1aa764
Zach Holcomb <zachholcombmusic@gmail.com>
2020-03-02 20:40:41 -0700

    Add crack.rb runner file, relocate runner files to their own folder outside of lib

---
+++ runner_files/crack.rb
@@ -0,0 +1,11 @@
+require './lib/enigma'
+
+enigma = Enigma.new
+message_reader = File.open(ARGV[0], "r")
+message_to_decrypt = message_reader.read.chomp
+require "pry"; binding.pry
+decrypted_file = File.open(ARGV[1], "w")
+output = enigma.crack(message_to_decrypt, ARGV[2])
+decrypted_file.write(output[:decryption])
+
+puts "Created #{ARGV[1]} with the cracked key #{output[:key]} and date #{output[:date]}"
